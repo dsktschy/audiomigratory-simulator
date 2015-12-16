e7004d19b4adeba5a4d6dcb441e6634743c971d3
dai <chamestica@gmail.com>
2015-12-16 17:34:57 +0900

    Read the fake data beforehand

--- src/javascripts/ams-data-fake.es6.js
+++ src/javascripts/ams-data-fake.es6.js
@@ -8,24 +8,32 @@ const
   /** 本来のデータ取得処理を模倣するための応答遅延時間(msec) */
   DELAY = 3000;
 
-var init, getParsedData;
+var init, getParsedData, isAJAXSimulating;
 
 /**
  * データ取得とパースを模倣する
+ *   script要素が予め存在していなかった場合はAJAXを模倣する
  * @exports
  */
 getParsedData = (params, onSuccess, onError) => {
-  setTimeout(onSuccess.bind(null, FAKE_DATA), DELAY);
+  if (isAJAXSimulating) {
+    setTimeout(onSuccess.bind(null, FAKE_DATA), DELAY);
+    return;
+  }
+  onSuccess(FAKE_DATA);
 };
 
 /**
  * module起動
- *   grobalに変数FAKE_DATAを生成するscriptタグを追加
+ *   grobal変数FAKE_DATAが存在しない場合はそれを定義するscript要素を追加
  *   ブラウザから非推奨の注意文言が出るがデバッグ時限定のためよしとする
  * @exports
  */
 init = () => {
-  $('head').append(HTML);
+  isAJAXSimulating = !FAKE_DATA;
+  if (isAJAXSimulating) {
+    $('head').append(HTML);
+  }
 };
 
 export default {
